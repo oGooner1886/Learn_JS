adc6c654195bb510ad0f5d97cb02c8989123293f
oGooner1886 <work.gooner1886@gmail.com>
2022-10-03 21:45:12 +0400

    array js

--- index.js
+++ index.js
@@ -152,19 +152,104 @@
 //     return c
 // }
 
-const x = (value, multiplier = 1) => {
-    return value * multiplier
-}
-console.log(x(10, 3))
-console.log(x(10))
+// const x = (value, multiplier = 1) => {
+//     return value * multiplier
+// }
+// console.log(x(10, 3))
+// console.log(x(10))
 
-const newPost = (post, addedAt = Date()) => ({
-    ...post,
-    addedAt,
-})
+// const newPost = (post, addedAt = Date()) => ({
+//     ...post,
+//     addedAt,
+// })
 
-const firstPost = {
-    id: 1,
-    author: 'Sergey',
-}
-console.table(newPost(firstPost))
+// const firstPost = {
+//     id: 1,
+//     author: 'Sergey',
+// }
+// console.table(newPost(firstPost))
+//! ВЫЛОВ ОШИБОК
+// const fnWithError = () => {
+//     throw new Error('Some error')
+// }
+// try {
+// fnWithError()
+// } catch (error) {
+// console.error(error);
+// console.log(error.message);
+
+// }
+// console.log('Continue...');
+//! INSTRUCTION     инструкция может заканчива тся точкой с запятой, если закрывается фигурной скобкой, то не нужно
+// let a;
+// const b = 5;
+// if (a > b) {
+//     console.log('a is larger');
+// }
+// for (let i = 0; i++; i < 5) {
+//     console.log(i);
+    
+// }
+// function myFn(a) {
+//     console.log(a);
+// }
+// const b = 3;
+// let c = 10;
+// myFn(2 + 3)
+// myFn(b)
+// myFn(c = c + 1)
+//! ARRAY - МАССИВ
+//?------------------
+//! 1 Вариант 
+// const myArray = [
+//     1,
+//     2,
+//     3,
+// ]
+// console.log(myArray);
+//! 2 Вариант
+// const myArray2 = new Array(1, 2, 3)
+// console.log(myArray2)
+                                    //todo myArray !== myArray2 т.к. разные ссылки у объектов
+    //todo МЕТОДЫ МАССИВОВ
+    //todo push, pop, shift, unshift, forEach, map
+    //todo PUSH - добавляет элемент в конец массива
+                //* const myArray = [1, 2, 3]
+                //* myArray.push('zxc')
+                //* console.log(myArray);
+    //todo POP - удаляет элемент из конца массива
+                //*  const myArray = [1, 2, 3]
+                //*  myArray.pop()
+                //*  console.log(myArray);
+            //todo - можно добавить удаленный из конца массива элемент в другой массив
+                //* const removedElement = myArray.pop()
+                //* console.log(myArray);
+                //* console.log(removedElement);
+    //todo UNSHIFT - добавляет элемент в начало массива
+                //*  const myArray = [1, 2, 3]
+                //*  myArray.unshift('Sergey')
+                //*  console.log(myArray);
+    //todo SHIFT - удаляет первый элемент из начала массива
+                //*  const myArray = [1, 2, 3]
+                //*  myArray.shift()
+                //*  console.log(myArray);            
+            //todo - можно добавить удаленный из конца массива элемент в другой массив
+                //* const removedElement = myArray.shift()
+                //* console.log(myArray);
+                //* console.log(removedElement);    
+    //todo forEach - перебирает все элементы массива
+                //*   const myArray = [1, 2, 3]
+                //*   myArray.forEach(el =>  console.log(el * 2))   //! выводится 2 4 6 (каждый элемент умножается на 2)
+                //*   console.log(myArray);                         //! оригинальный массив не изменился
+    //todo MAP - перебирает все элементы массива и возвращает их в новый массив, при этом оригинальный массив не меняется
+                //*  const myArray = [1, 2, 3]
+                //*  const newArray = myArray.map(el => el * 3)     
+                //*  console.log(newArray);
+                //*  console.log(myArray);                          //! оригинальный массив не изменился
+                
+                //*  аналог стрелочной функции с явным объявлением
+                //*  const newArray = myArray.map(function (el) {
+                //*  return el * 3   
+                //*  })
+                 
+    
